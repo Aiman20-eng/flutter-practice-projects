880977e220d779a9328d68e7bac2b9ed02ace925
aiman almoliki <aimnalmoliki072@gmail.com>
2026-04-09 23:41:00 +0300

    Update todo app with latest local changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:f_1/feature/home/views/welcome_pageview.dart';
-void main(){
+
+void main() {
   runApp(MyApp());
 }
 
@@ -8,19 +9,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner:false,
-     home: welcome_pageview(
-
-      ),
+      debugShowCheckedModeBanner: false,
+      home: welcome_pageview(),
     );
   }
 }
+// we are sdtoped in 20:00 from  https://youtu.be/pk1WMkA6TFk?si=RmQB-bb-MxQAIP64
 
 class WelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor:  const Color.fromARGB(255, 60, 115, 142),
-    );
+    return Scaffold(backgroundColor: const Color.fromARGB(255, 60, 115, 142));
   }
 }
